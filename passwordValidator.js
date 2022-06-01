03bf4dcf41c284a3428bc357919f789b3e548f30
hisahito <hisahito706@gmail.com>
2022-05-31 23:08:15 -0500

    adding isPasswordValid function

--- passwordValidator.js
+++ passwordValidator.js
@@ -11,7 +11,26 @@ function isValidPassword(password){
         };
     }
     const isStrong = 
-        /^(?=.*[0-9])(?=.*[!@#$%^&'|_>`()])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*'|_>`()]{16,32}/.test(password);
+        /^(?=.*[0-9])(?=.*[!@#$%^&'|_>`()])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*'|_>`()]{16,32}/.test(
+            password
+            );
     const hasTwoSpecialChars =
-        /(?:[^'!])
+        /(?:[^`!@#$%^&*\-_=+'/.,|><()]*[`!@#$%^&*\-_=+'/.,|><()]){2}/.test(
+            password
+        );
+    const charsSequences = hasSequence(password, 3);
+    if(charsSequences.length > 2) {
+        return {
+            result: false,
+            reason: `3 chars in sequence: ${charsSequences}`,
+        };
+    } else if (!hasTwoSpecialChars){
+        return {result: false, reason: "Missing special chars" };
+    } else if (!isStrong){
+        return {
+            result: false,
+            reason: "Its must contain lowercase and uppercase letters, number and 2 special chars.",
+        };
+    }
+    return {result: true};
 }
