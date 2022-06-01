bdb71d81c28aebc1a1233e1ee5cf1c3bca01208c
hisahito <hisahito706@gmail.com>
2022-05-31 23:31:41 -0500

    adding the new function hasSequence

--- passwordValidator.js
+++ passwordValidator.js
@@ -1,3 +1,25 @@
+function hasSequence(str, max){
+    if(str) {
+        let letters = "abcdefghijklmnñopqrstuvwxyz";
+        let numbers = "0123456789";
+
+        const pattern = `(?<letters>[a-zA-Z]{${max},})|(?<numbers>[0-9]{${max},})`;
+        const regEx = new RegExp(pattern, "g");
+
+        let match;
+        while ((match = regEx.exec(str))) {
+            const chars = match.groups.letters ? letters : numbers;
+            const strToCheck = match.groups.letters
+                ? match.groups.letters
+                : match.groups.numbers;
+            const sequence = getSequence(chars, strToCheck, max);
+            if(sequence.length >= max) {
+                return sequence;
+            }
+        }
+    }
+    return "";
+}
 function isValidPassword(password){
     if(password.length < 16) {
         return {
@@ -11,11 +33,11 @@ function isValidPassword(password){
         };
     }
     const isStrong = 
-        /^(?=.*[0-9])(?=.*[!@#$%^&'|_>`()])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*'|_>`()]{16,32}/.test(
+        /^(?=.*[0-9])(?=.*[!@#$%^&*'|_>`()])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*'|_>`()]{16,32}/.test(
             password
             );
     const hasTwoSpecialChars =
-        /(?:[^`!@#$%^&*\-_=+'/.,|><()]*[`!@#$%^&*\-_=+'/.,|><()]){2}/.test(
+        /(?:[^`!@#$%^&*\-_=+'.,|><()]*[`!@#$%^&*\-_=+'.,|><()]){2}/.test(
             password
         );
     const charsSequences = hasSequence(password, 3);
